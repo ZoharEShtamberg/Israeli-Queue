02cd0857adc85a66d16e872283847ce4168d7cbb
s-Danielle <sdanielle@campus.technion.ac.il>
2023-04-21 00:59:54 +0300

    implemented some more functions

--- HackerEnrollment.c
+++ HackerEnrollment.c
@@ -5,27 +5,27 @@
 #include <string.h>
 
 
-#define ID_LENGTH 9
+#define ID_LENGTH 10
+#define MAX_STRING_LEN 100
+#define COURSE_NUM_LENGTH 6
+
 //=================================================================
 /*typedefs & struct declaration*/
 //=================================================================
+typedef struct Node_t{
+	void *m_item;
+	struct node_t *m_next;
+} *Node;
 
 typedef struct EnrollmentSystem_t{
+
     Student m_studentsHeadNode;
     Course m_coursesHeadNode;
-    int m_studentsNum, m_coursesNum;//do we need this? i really dont know i miss arrays what is this node shit
+
+    //int m_studentsNum, m_coursesNum, m_hackersNum;
+    //do we need this? i really dont know i miss arrays what is this node shit
 
 }*EnrollmentSystem;
-
-/*typedef struct Hacker_t{
-    char m_studentID[10];
-    Course *m_desiredCourses;
-    Student *m_friends, *m_enemies;
-
-    
-
-}*Hacker;
-*/
 
 typedef struct Student_t{
 
@@ -42,6 +42,13 @@ typedef struct Course_t{
     int m_number, m_size;
 }*Course;
 
+//=================================================================================
+//Inner Functions Declerations:
+//=================================================================================
+
+int fileLength(FILE* file);
+Node* createStudentListFromFile(FILE* studentsFile);
+Node* createSCoursesListFromFile(FILE* coursesFile);
 
 
 //==================================================================================
@@ -54,12 +61,28 @@ typedef struct Course_t{
  * @param courses: <Course Number> <size>\n
  * @param hackers: <Student ID>\n <Course Number>\n <Student ID>\n (friends) <Student ID>\n (rivals)
  * 
- * 
+ * ALL MALLOCS SHOULD BE FREED IN DESTROY FUCTION??
  * returns a pointer
  * */
 EnrollmentSystem createEnrollment(FILE* students, FILE* courses, FILE* hackers){
-
     
+    if(!students||!courses||!hackers){
+        return NULL;//BAD PARAM
+    }
+
+    EnrollmentSystem newSys=malloc(sizeof(EnrollmentSystem));
+    Node studentsListHead=createStudentListFromsFile(students);
+    Node coursesListHead=createSCoursesListFromFile(courses);
+
+    if (!newSys||!studentsListHead||!coursesListHead){
+        return NULL;//MALLOC FAIL
+    }
+    /*
+    code: update students by hackers file
+    */
+
+    newSys->m_coursesHeadNode=coursesListHead;
+    newSys->m_studentsHeadNode=studentsListHead;   
 }
 
 /**Function Description:
@@ -67,11 +90,127 @@ EnrollmentSystem createEnrollment(FILE* students, FILE* courses, FILE* hackers){
  * */
 EnrollmentSystem readEnrollment(EnrollmentSystem sys, FILE* queues);
 
-
 /**Function Description:
  * 
  * */
 void hackEnrollment(EnrollmentSystem sys, FILE* out);
+
+
+//=========================================================================
+//Inner Functions
+//=========================================================================
+
+
+//reads student file return null for error
+//all memeory allocations should be freed in destroy unless given null
+Node* createStudentListFromFile(FILE* studentsFile){
+
+    Node head=malloc(sizeof(Node));
+    Node tail=malloc(sizeof(Node));
+    if(!head||!tail){
+        return NULL;//malloc fail
+    }
+    head=NULL;
+
+    int totalCredits;
+    double GPA;
+    char ID[ID_LENGTH], FirstName[MAX_STRING_LEN], lastName[MAX_STRING_LEN], city[MAX_STRING_LEN], department[MAX_STRING_LEN];
+    
+    while(scanf(studentsFile,"%9s %d %lf %99s %99s %99s %99s", ID, &totalCredits, &GPA, FirstName, lastName, city, department)==7)
+    {
+        if (!studentsFile){
+            return NULL; //BAD PARAM
+        }
+        Student currentStudent=(Student)malloc(sizeof(Student));
+        
+        if(!currentStudent){
+            return NULL;//MALLOC FAIL
+        }
+
+        strcpy(currentStudent->m_studentID,ID);
+        strcpy(currentStudent->m_city,city);
+        strcpy(currentStudent->m_department,department);
+        strcpy(currentStudent->m_firstName,FirstName);
+        strcpy(currentStudent->m_lastName,lastName);
+        currentStudent->m_GPA=GPA;
+        currentStudent->m_totalCredits=totalCredits;
+        currentStudent->m_friends=NULL;
+        currentStudent->m_enemies=NULL;
+        
+
+        Node nextNode=malloc(sizeof(Node));
+        if(!nextNode){
+            return NULL;//MALLOC FAIL
+        }
+        nextNode->m_item=currentStudent;
+        nextNode->m_next=NULL;
+
+        if (head==NULL){
+            head=nextNode;
+        }
+        else{
+            tail->m_next=nextNode;
+        }
+        tail=nextNode;
+    }
+
+    return head;
+
+
+}
+
+
+Node* createSCoursesListFromFile(FILE* coursesFile){
+    if (!coursesFile){
+        return NULL; //BAD PARAM
+    }
+    Node head=malloc(sizeof(Node));
+    Node tail=malloc(sizeof(Node));
+    if(!head||!tail){
+        return NULL;//malloc fail
+    }
+    head=NULL;
+
+    int courseNumber, courseSize;
+    while(fscanf(coursesFile,"%d %d",&courseNumber, &courseSize)==2){
+        Course newCourse=malloc(sizeof(Course));
+        if(!newCourse){
+            return NULL;//MALLOC FAIL
+        }
+        newCourse->m_number=courseNumber;
+        newCourse->m_size=courseSize;
+
+        Node newNode=malloc(sizeof(Node));
+        if(!newNode){
+            return NULL;//MALLOC FAIL
+        }
+
+        if (head==NULL){
+            head=newNode;
+        }
+        else{
+            tail->m_next=newNode;
+        }
+        tail=newNode;
+    }
+}
+
+
+
+//returns file length and doesnt change the file ptr
+int fileLength(FILE* file){
+    fseek(file,0,SEEK_END);
+    int length=ftell(file);
+    fseek(file, 0, SEEK_SET);
+    return length;
+
+}
+
+
+
+
+
+
 
 
 
@@ -80,7 +219,7 @@ void hackEnrollment(EnrollmentSystem sys, FILE* out);
 //==================================================================================
 
 
-//checks if 
+//TODO: rewrite this whole thing checks if 
 int friendshipByHackerFile(Student stu, Student hacker){
 
     if (stu==NULL||hacker==NULL){
