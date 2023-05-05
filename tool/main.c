9bcc633519b1489c9f7a5103c587d80f40d9fa31
zohar <z@s>
2023-05-05 15:38:47 +0300

    changed main

--- tool/main.c
+++ tool/main.c
@@ -8,22 +8,39 @@
 
 int main(int argc, char** argv){
 	if(argc<6 || argc>7){
-		return -1;//incorrect usage!
+		return -0;//incorrect usage!
 	}
 	bool ignoreUpperFlag = (strcmp(argv[1], "-i")) ? false: true; //inverse of strcmp
 	int i=(ignoreUpperFlag) ? 2 : 1;
 	FILE* students = fopen(argv[i++], "r");
+	if(students==NULL){
+		return 0;
+	}
 	FILE* courses = fopen(argv[i++], "r");
+	if(courses==NULL){
+		fclose(students);
+		return 0;
+	}
 	FILE* hackers = fopen(argv[i++], "r");
+	if(hackers==NULL){
+		fclose(students);
+		fclose(courses);
+		return 0;
+	}
 	FILE* queues = fopen(argv[i++], "r");
+	if(queues==NULL){
+		fclose(students);
+		fclose(courses);
+		fclose(hackers);
+		return 0;
+	}
 	FILE* target = fopen(argv[i++], "w");
-	if(!students || !courses || !hackers || !queues || !target){
-		students && fclose(students);
-		courses && fclose(courses);
-		hackers && fclose(hackers);
-		queues && fclose(queues);
-		target && fclose(target);
-		return -1;
+	if(target==NULL){
+		fclose(students);
+		fclose(courses);
+		fclose(hackers);
+		fclose(queues);
+		return 0;
 	}
 	EnrollmentSystem sys = createEnrollment(students, courses, hackers);
 	if(ignoreUpperFlag){
